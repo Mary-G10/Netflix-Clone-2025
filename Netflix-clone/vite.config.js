e476f63639a1239cc2238fcbc3910901ebbd78f3
Mary-G10 <merongeremew@yahoo.com>
2025-10-23 14:13:19 -0500

    Add _redirects file for Netlify deployment

--- Netflix-clone/vite.config.js
+++ Netflix-clone/vite.config.js
@@ -1,11 +1,11 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone-2025/",
+  base: "/",
   build: {
     outDir: "dist",
   },
 });
+
